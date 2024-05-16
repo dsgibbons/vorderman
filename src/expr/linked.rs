c8c637a6eb519a5621f493c94671c583db73e324
dsgibbons <git@gibbons.foo>
2024-05-17 07:30:13 +0930

    feat: LinkedExpression::from_str

--- src/expr/linked.rs
+++ src/expr/linked.rs
@@ -1,5 +1,7 @@
 use super::op::{Operand, Operation};
+use super::parse;
 use std::fmt;
+use std::str::FromStr;
 
 pub type Node = Operand<LinkedExpression>;
 
@@ -24,6 +26,39 @@ impl fmt::Display for LinkedExpression {
             Some(e) => write!(f, "{} {}", self.0, e),
             None => write!(f, "{}", self.0),
         }
+    }
+}
+
+impl FromStr for LinkedExpression {
+    type Err = parse::ParseExpressionError;
+
+    fn from_str(input: &str) -> Result<LinkedExpression, Self::Err> {
+        let input = input.trim();
+
+        let mut next_index = 0;
+        let mut next_operand: parse::NextObjectFromStringStart<Operand<LinkedExpression>>;
+        let mut next_operation: parse::NextObjectFromStringStart<Operation>;
+
+        let next_operand = parse::find_next_operand(&input[next_index..]).unwrap();
+
+        next_index += next_operand.end_char_index + 1;
+
+        let edge: Option<Edge>;
+        if next_index < input.len() - 1 {
+            next_operation = parse::find_next_operation(&input[next_index..]).unwrap();
+            next_index += next_operation.end_char_index + 1;
+            if next_index == input.len() {
+                return Err(parse::ParseExpressionError::NotEnoughOperations);
+            };
+            edge = Some(Edge {
+                operation: next_operation.object,
+                operand: Box::new(LinkedExpression::from_str(&input[next_index..]).unwrap()),
+            });
+        } else {
+            edge = None;
+        }
+
+        Ok(LinkedExpression(next_operand.object, edge))
     }
 }
 
@@ -81,5 +116,25 @@ mod tests {
         );
 
         assert_eq!(outer_expr.to_string(), "1 + (2 - 3) * 4 / 5");
+    }
+
+    macro_rules! str_to_linked_expr_tests {
+        ($($name:ident: $value:expr,)*) => {
+        $(
+            #[test]
+            fn $name() {
+                let (input, expected) = $value;
+                assert_eq!(LinkedExpression::from_str(input).unwrap().to_string(), expected);
+            }
+        )*
+        }
+    }
+
+    str_to_linked_expr_tests! {
+        str_to_link_0: ("1", "1"),
+        str_to_link_1: ("1 + 2", "1 + 2"),
+        str_to_link_2: ("(1)", "(1)"),
+        str_to_link_3: ("(1*2 ) + (3 - (4/ 5)) + 6 /((7) - (8 * (  9 +10 )))   ", "(1 * 2) + (3 - (4 / 5)) + 6 / ((7) - (8 * (9 + 10)))"),
+        str_to_link_4: ("((      4 + 5     ))", "((4 + 5))"),
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,8 @@ struct NumbersRound {
     numbers: Vec<u32>,
     target: Ratio<u32>,
 }
+
+// construct solutions
+// random generate expressions for testing
+// test solutions can be evaluated
+// DFS for building solutions towards target
