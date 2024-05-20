2c0395d3ac776b8fcc62be2fd7c4b9cc5baff51d
dsgibbons <git@gibbons.foo>
2024-05-21 07:04:32 +0930

    refactor: expr methods to be better encapsulated

--- src/expr/expr.rs
+++ src/expr/expr.rs
@@ -1,18 +1,13 @@
 use std::fmt;
+use std::str::FromStr;
 use std::vec::Vec;
 
 const BASE: u32 = 10;
 
 #[derive(Debug, PartialEq)]
-struct Expression {
-    tokens: Vec<Token>,
-}
-
-#[derive(Debug, PartialEq)]
-enum Token {
-    Number(u32),
-    Operation(Operation),
-    Parenthesis(Parenthesis),
+pub enum LexError {
+    InvalidCharacter(char),
+    InvalidCharacterAtIndex(usize, char),
 }
 
 #[derive(Debug, PartialEq)]
@@ -21,33 +16,6 @@ pub enum Operation {
     Subtract,
     Multiply,
     Divide,
-}
-
-#[derive(Debug, PartialEq)]
-enum Parenthesis {
-    Open,
-    Close,
-}
-
-impl fmt::Display for Expression {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut characters = Vec::new();
-        for c in self.tokens.iter() {
-            characters.push(c.to_string());
-        }
-
-        write!(f, "{}", characters.join(" "))
-    }
-}
-
-impl fmt::Display for Token {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Token::Number(n) => write!(f, "{}", n),
-            Token::Operation(op) => write!(f, "{}", op),
-            Token::Parenthesis(p) => write!(f, "{}", p),
-        }
-    }
 }
 
 impl fmt::Display for Operation {
@@ -63,6 +31,26 @@ impl fmt::Display for Operation {
     }
 }
 
+impl TryFrom<char> for Operation {
+    type Error = LexError;
+
+    fn try_from(input: char) -> Result<Operation, Self::Error> {
+        match input {
+            '+' => Ok(Operation::Add),
+            '-' => Ok(Operation::Subtract),
+            '*' => Ok(Operation::Multiply),
+            '/' => Ok(Operation::Divide),
+            c => Err(LexError::InvalidCharacter(c)),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum Parenthesis {
+    Open,
+    Close,
+}
+
 impl fmt::Display for Parenthesis {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let c = match self {
@@ -74,48 +62,88 @@ impl fmt::Display for Parenthesis {
     }
 }
 
-#[derive(Debug, PartialEq)]
-enum LexError {
-    InvalidCharacter(usize, char),
+impl TryFrom<char> for Parenthesis {
+    type Error = LexError;
+
+    fn try_from(input: char) -> Result<Parenthesis, Self::Error> {
+        match input {
+            '(' => Ok(Parenthesis::Open),
+            ')' => Ok(Parenthesis::Close),
+            c => Err(LexError::InvalidCharacter(c)),
+        }
+    }
 }
 
-fn lex(input: &str) -> Result<Expression, LexError> {
-    let mut tokens = Vec::<Token>::new();
-    let mut num_buffer = Vec::<char>::new();
-    for (i, c) in input.trim().char_indices() {
-        if c.is_digit(BASE) {
-            num_buffer.push(c);
-            continue;
-        } else if num_buffer.len() > 0 {
+#[derive(Debug, PartialEq)]
+enum Token {
+    Number(u32),
+    Operation(Operation),
+    Parenthesis(Parenthesis),
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Token::Number(n) => write!(f, "{}", n),
+            Token::Operation(op) => write!(f, "{}", op),
+            Token::Parenthesis(p) => write!(f, "{}", p),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct Expression {
+    tokens: Vec<Token>,
+}
+
+impl fmt::Display for Expression {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut characters = Vec::new();
+        for c in self.tokens.iter() {
+            characters.push(c.to_string());
+        }
+
+        write!(f, "{}", characters.join(" "))
+    }
+}
+
+impl FromStr for Expression {
+    type Err = LexError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut tokens = Vec::<Token>::new();
+        let mut num_buffer = Vec::<char>::new();
+        for (i, c) in s.trim().char_indices() {
+            if c.is_digit(BASE) {
+                num_buffer.push(c);
+                continue;
+            } else if num_buffer.len() > 0 {
+                tokens.push(Token::Number(
+                    num_buffer.iter().collect::<String>().parse().unwrap(),
+                ));
+                num_buffer = Vec::<char>::new();
+            }
+
+            let token: Option<Token> = match c {
+                ' ' => None,
+                '(' | ')' => Some(Token::Parenthesis(Parenthesis::try_from(c).unwrap())),
+                '+' | '-' | '*' | '/' => Some(Token::Operation(Operation::try_from(c).unwrap())),
+                _ => return Err(LexError::InvalidCharacterAtIndex(i, c)),
+            };
+
+            match token {
+                Some(t) => tokens.push(t),
+                None => continue,
+            }
+        }
+
+        if num_buffer.len() > 0 {
             tokens.push(Token::Number(
                 num_buffer.iter().collect::<String>().parse().unwrap(),
             ));
-            num_buffer = Vec::<char>::new();
         }
-
-        let token: Option<Token> = match c {
-            ' ' => None,
-            '(' => Some(Token::Parenthesis(Parenthesis::Open)),
-            ')' => Some(Token::Parenthesis(Parenthesis::Close)),
-            '+' => Some(Token::Operation(Operation::Add)),
-            '-' => Some(Token::Operation(Operation::Subtract)),
-            '*' => Some(Token::Operation(Operation::Multiply)),
-            '/' => Some(Token::Operation(Operation::Divide)),
-            _ => return Err(LexError::InvalidCharacter(i, c)),
-        };
-
-        match token {
-            Some(t) => tokens.push(t),
-            None => continue,
-        }
+        Ok(Expression { tokens })
     }
-
-    if num_buffer.len() > 0 {
-        tokens.push(Token::Number(
-            num_buffer.iter().collect::<String>().parse().unwrap(),
-        ));
-    }
-    Ok(Expression { tokens })
 }
 
 #[cfg(test)]
@@ -128,7 +156,7 @@ mod tests {
             #[test]
             fn $name() {
                 let (input, tokens) = $value;
-                assert_eq!(lex(input).unwrap(), Expression { tokens });
+                assert_eq!(Expression::from_str(input).unwrap(), Expression { tokens });
             }
         )*
         }
@@ -145,7 +173,10 @@ mod tests {
     #[test]
     fn bad_lex_char() {
         let input = "(1+ 2/ 3** a 51 x)";
-        assert_eq!(lex(input), Err(LexError::InvalidCharacter(11, 'a')))
+        assert_eq!(
+            Expression::from_str(input),
+            Err(LexError::InvalidCharacterAtIndex(11, 'a'))
+        )
     }
 
     macro_rules! expr_to_str_tests {
@@ -154,7 +185,7 @@ mod tests {
             #[test]
             fn $name() {
                 let (input, expected) = $value;
-                let as_expr = lex(input).unwrap().to_string();
+                let as_expr = Expression::from_str(input).unwrap().to_string();
                 assert_eq!(as_expr, expected.to_string());
             }
         )*
